8ff70d572a389411a1160480ebad73c53e5fd045
Eku2499 <ekawadeinfotech@gmail.com>
2025-11-06 15:35:10 +0530

    Delete Ekawade Infotech/start_prod.sh

--- Ekawade Infotech/start_prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Production start: ensure DB initialized and start gunicorn
-export FLASK_ENV=production
-export BOOKING_DB=${BOOKING_DB:-"$(pwd)/bookings.db"}
-python -c "from booking_api import init_db; init_db(migrate_from_excel=False)"
-exec gunicorn -w 4 -b 0.0.0.0:8000 booking_api:app
