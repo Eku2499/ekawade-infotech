ae083751d1acb1671afa78a975b28371aacd3320
Eku2499 <ekawadeinfotech@gmail.com>
2025-11-06 15:31:47 +0530

    Delete Ekawade Infotech/run_waitress.ps1

--- Ekawade Infotech/run_waitress.ps1
+++
@@ -1,4 +0,0 @@
-# Run this on Windows PowerShell to serve with Waitress
-# Usage: .\run_waitress.ps1
-python -m pip install waitress -q
-python -c "from waitress import serve; import booking_api; serve(booking_api.app, host='0.0.0.0', port=8000)"
